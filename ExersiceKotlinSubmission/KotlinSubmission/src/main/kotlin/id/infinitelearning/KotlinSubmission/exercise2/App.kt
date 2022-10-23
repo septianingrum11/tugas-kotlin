7e1c4dee7c54b503a9eac556a53e2e0afba97214
septianingrum11 <115516153+septianingrum11@users.noreply.github.com>
2022-10-23 19:37:37 +0700

    tugas

---
+++ ExersiceKotlinSubmission/KotlinSubmission/src/main/kotlin/id/infinitelearning/KotlinSubmission/exercise2/App.kt
@@ -0,0 +1,62 @@
+package id.infinitelearning.KotlinSubmission.exercise2
+
+fun main(){
+
+    /** Latihan 1 List
+    Buatlah sebuah variabel bertipe list dengan ketentuan:
+    - List tersebut menampung bilangan genap dari 1 hingga 100
+    Optional:
+    - Agar lebih mudah, gunakanlah for loop dan logika if untuk mengisi bilangan genap pada list
+     */
+    val numbers = mutableListOf<Int>()
+    for (i in 1..100) {
+        if (i % 2 == 0){
+            numbers.add(i);
+        }
+    }
+
+    println(numbers)
+
+
+
+
+
+/** Latihan 2 Map
+    Buatlah variabel bertipe Map dengan kriteria:
+    Daftar nama bulan dalam setahun,
+    - Set Key nya dengan inisial nama bulan dan Value nya nama bulan, contoh
+    - key "Jan", value January
+    - key "Feb", value February
+    - dst...
+    Cetak semua nama bulan dengan fungsi forEach sehingga hasil output seperti:
+    - Jan -> January
+    - Feb -> February
+    - Dst...
+
+    Cetak nama bulan sekarang dan bulan lahir kamu dengan format string berikut:
+    - "It's {$monthNow} now, I was born in {$birthMonth}"
+     */
+
+        val months = mapOf(
+            "Jan" to "January",
+            "Feb" to "February",
+            "Mar" to "March",
+            "Apr" to "April",
+            "Mei" to "Mei",
+            "Jul" to "Juli",
+            "Ags" to "Agustus",
+            "Sept" to "September",
+            "Oct" to "October",
+            "Nov" to "November",
+            "Des" to "Desember",
+        )
+
+        println(months)//
+        months.forEach{ value ->
+            println("$value")
+        }
+        val monthNow = months["Oct"]
+        val birthMonth = months["Sept"]
+        println("it's {$monthNow} now, Saya Lahir Bulan {$birthMonth}")
+
+}
---
+++ exercise4/App.kt
@@ -0,0 +1,15 @@
+package id.infinitelearning.KotlinSubmission.exercise4
+
+fun validateAccount(name: Any, password: String): Any {
+
+
+
+fun main() {
+
+    /**
+     * Buatlah contoh code penggunaan try catch untuk menangani suatu kasus sesuai keinginan teman-teman
+     *
+     */
+
+    val result = validateAccount( name = "Septia", password = "123456")
+}
